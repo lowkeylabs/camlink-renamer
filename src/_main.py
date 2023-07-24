8ca32e26f4bce25d024e556121581510d2a8672f
John Leonard <jdleonard@vcu.edu>
2023-07-24 13:21:11 -0400

    Got list working. Now for rename ...

--- src/_main.py
+++ src/_main.py
@@ -4,10 +4,10 @@ import sys
 import winreg
 import click
 from loguru import logger
-from .utils import rename_device_instance, search_registry_for_device_instance, rename_device_driver
+from .utils import rename_device_driver, get_camlink_devices
 
 logger.remove()
-logger.add(sys.stderr, level="INFO")
+logger.add(sys.stderr, level="SUCCESS")
 
 
 @click.group()
@@ -21,21 +21,24 @@ def cli(ctx):
 @click.option("--all", help="Show all CamLinks ever connected to system.")
 @click.option("-v","--verbose", help="Provide detailed / verbose output")
 @click.pass_context
-def list(ctx):
-    """ List CamLink devices and provides simple ID (sid).
+def list(ctx,all,verbose):
+    """ Lists Elgato CamLink devices and provides a simple reference id.
     """
     print("list")
 #    device_instance_path_to_search = "USB#VID_0FD9&PID_0066&MI_00#8&2764D13E&0&0000"
-#    keylist = search_registry_for_device_instance(device_instance_path_to_search)
-#    rename_device_instance("Cam Link 4K-Canon R10",keylist)
-#    rename_device_driver( device_instance_path_to_search )
+
+    camlink_list = get_camlink_devices()
+    i = 0
+    for camlink in camlink_list.keys():
+        print( f"{i:2} : {camlink_list[camlink]['device_id']:8} : {camlink_list[camlink]['friendly_name']}" )
+        i = i + 1
 
 @cli.command()
 @click.pass_context
 @click.option("--sid",help="The list-ID of CamLink from previous list command")
 @click.option("--name", help="The friendly name to associate with this CamLink")
-def rename(ctx):
-    """ Rename CamLink device using simple ID (sid).
+def rename(ctx,sid,name):
+    """ Renames CamLink device using reference id
     """
     print("rename")
 #    device_instance_path_to_search = "USB#VID_0FD9&PID_0066&MI_00#8&2764D13E&0&0000"
--- src/utils.py
+++ src/utils.py
@@ -3,7 +3,7 @@
 import os
 import sys
 import winreg
-
+from loguru import logger
 
 def search_registry_for_device_instance( substring ):
     """ Search registry """
@@ -20,7 +20,7 @@ def search_registry_for_device_instance( substring ):
         sys.exit(1)
 
     logger.debug("searching subkeys")
-    key_list = []
+    key_list = {}
     try:
         count = winreg.QueryInfoKey(key)[0]
 
@@ -38,16 +38,31 @@ def search_registry_for_device_instance( substring ):
                     subsubkey_name = winreg.EnumKey(subkey, j)
 #                    logger.debug(f"{registry_path}\\{subkey_name}\\{subsubkey_name}")
 #                    logger.debug(f"{subsubkey_name}")
-#                    logger.debug(f"{subsubkey_name}")
                     if substring.upper() in subsubkey_name.upper():
+#                        logger.info(f"subsubkey: {subsubkey_name}")
 
                         try:
                             searchkey_path = f"{registry_path}\\{subkey_name}\\{subsubkey_name}\\#GLOBAL\\Device Parameters"
                             logger.debug(f"{searchkey_path}")
                             searchkey = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE,searchkey_path)
                             friendly_name, itype = winreg.QueryValueEx( searchkey,"FriendlyName")
-                            logger.info(f"{friendly_name} found in found in: {searchkey_path}")
-                            key_list.append( searchkey_path )
+                            logger.info(f"{friendly_name} found in found in: {subsubkey_name}")
+            
+                            parts = searchkey_path.split("\\")
+                            moreparts = parts[5].split("#")
+                            device_instance_path = f"{moreparts[3]}#{moreparts[4]}#{moreparts[5]}"
+                            device_identifier = moreparts[5]
+                            moremoreparts = device_identifier.split("&")
+                            device_id = moremoreparts[1]
+
+                            if not device_id in key_list.keys():
+                                key_list[device_id] = {"device_id":device_id,"friendly_name":friendly_name,"searchkey_paths":[]}
+                            key_list[device_id]["searchkey_paths"].append({"searchkey_path":searchkey_path,
+                                                        "friendly_name":friendly_name,
+                                                        "subsubkey_name":subsubkey_name,
+                                                        "device_instance_path":device_instance_path,
+                                                        "device_identifier":device_identifier,
+                                                        "device_id":device_id})
                             winreg.CloseKey( searchkey )
                         except Exception as e:
                             logger.debug(f"Error with search: {e}")
@@ -83,3 +98,25 @@ def rename_device_driver( long_path ):
     print(f"{long_path}")
     # split on \# character. Create 3 pieces.
     # driver friendly name is here: Computer\HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Enum\#1\#2\#3\FriendlyName
+
+
+def get_camlink_devices():
+    """ return list of camlink devices
+    """
+
+# USB\VID_0FD9&PID_0067&MI_00\8&298320BB&0&0000
+# USB\VID_0FD9&PID_0066&MI_00\6&1D3A44BA&0&0000
+# USB\VID_0FD9&PID_0066&MI_00\6&86946DA&0&0000
+
+    device_instance_path_to_search = "USB#VID_0FD9&PID_0066"
+    keylist1 = search_registry_for_device_instance(device_instance_path_to_search)
+    device_instance_path_to_search = "USB#VID_0FD9&PID_0067"
+    keylist2 = search_registry_for_device_instance(device_instance_path_to_search)
+
+    for key in keylist2.keys():
+        keylist1[key] = keylist2[key]
+
+#    rename_device_instance("Cam Link 4K-Canon R10",keylist)
+#    rename_device_driver( device_instance_path_to_search )
+
+    return keylist1
